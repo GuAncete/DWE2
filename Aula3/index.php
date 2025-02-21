db4611e12ece0b83b0bbbc1d035ae261343878dd
Gustavo_An7 <gustavoaancete@gmail.com>
2025-02-21 08:11:54 -0300

    Aula

---
+++ Aula3/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title></title>
+</head>
+
+<body>
+    <?php
+    echo "<h1>My Firt  PHP page</h1>";
+    echo "<p>Hello World!</p>";
+    echo "<br><p>Olá, meu nome é <strong>Eder<strong>, Hoje é dia <strong>19/02/2025<strong> e agora são <strong>09:26<strong></p>";
+    ?>
+
+</body>
+
+</html>
